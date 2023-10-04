651a5b5d03aa7035335e7772d4c828edae3030ae
KiHwan Yun <drg2524@naver.com>
2023-10-04 14:41:26 +0900

    Utils method file commit

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,5 +1,7 @@
 package hello.thymeleaf.basic;
+
 import lombok.Data;
+import lombok.Getter;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -28,7 +31,7 @@ public class BasicController {
     }
 
     @GetMapping("/variable")
-    public String variable(Model model){
+    public String variable(Model model) {
         User userA = new User("userA", 10);
         User userB = new User("userB", 20);
 
@@ -36,29 +39,38 @@ public class BasicController {
         list.add(userA);
         list.add(userB);
         Map<String, User> map = new HashMap<>();
-        map.put("userA" , userA);
-        map.put("userB" , userB);
+        map.put("userA", userA);
+        map.put("userB", userB);
 
-        model.addAttribute("user" , userA);
-        model.addAttribute("users" , list);
-        model.addAttribute("userMap" , map);
+        model.addAttribute("user", userA);
+        model.addAttribute("users", list);
+        model.addAttribute("userMap", map);
 
         return "basic/variable";
     }
 
     @GetMapping("/basic-objects")
-    public String basicObjects(HttpSession session){
-        session.setAttribute("sessionData" , "Hello Session");
+    public String basicObjects(HttpSession session) {
+        session.setAttribute("sessionData", "Hello Session");
 
         return "basic/basic-objects";
     }
 
+
     @Component("helloBean")
-    static class HelloBean{
-         public String hello(String data){
-             return "Hello " + data;
-         }
+    static class HelloBean {
+        public String hello(String data) {
+            return "Hello " + data;
+        }
     }
+
+    @GetMapping("/date")
+    public String date(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now());
+
+        return "basic/date";
+    }
+
     @Data
     static class User {
         private String username;
